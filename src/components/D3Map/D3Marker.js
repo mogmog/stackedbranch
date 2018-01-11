6db85e2d13e9e81b4f2b4a613f936f3a459ccc2f
Graham <mogmog@gmail.com>
2018-01-11 12:32:18 +0000

    move to d3 grid

--- src/components/D3Map/D3Marker.js
+++ src/components/D3Map/D3Marker.js
@@ -5,130 +5,39 @@ import 'leaflet.markercluster';
 import 'leaflet-d3-svg-overlay';
 import d3 from 'd3';
 
-Leaflet.VirtualGrid = Leaflet.FeatureGroup.extend({
-  include: Leaflet.Mixin.Events,
-  options: {
-    cellSize: 64,
-    delayFactor: 3.5,
-    style: {
-      stroke: true,
-      color: '#3ac1f0',
-      dashArray: null,
-      lineCap: null,
-      lineJoin: null,
-      weight: 2,
-      opacity: 1,
-
-      fill: true,
-      fillColor: null, //same as color by default
-      fillOpacity: 0.5,
-
-      clickable: true
-    }
-  },
-  initialize: function(options){
-    Leaflet.Util.setOptions(this, options);
-    Leaflet.FeatureGroup.prototype.initialize.call(this, [], options);
-  },
-  onAdd: function(map){
-    Leaflet.FeatureGroup.prototype.onAdd.call(this, map);
-    this._map = map;
-    this._cells = [];
-    this._setupGrid(map.getBounds());
-
-    map.on("move", this._moveHandler, this);
-    map.on("zoomend", this._zoomHandler, this);
-    map.on("resize", this._resizeHandler, this);
-  },
-  onRemove: function(map){
-    Leaflet.FeatureGroup.prototype.onRemove.call(this, map);
-    map.off("move", this._moveHandler, this);
-    map.off("zoomend", this._zoomHandler, this);
-    map.off("resize", this._resizeHandler, this);
-  },
-  _clearLayer: function(e) {
-    this._cells = [];
-  },
-  _moveHandler: function(e){
-    this._renderCells(e.target.getBounds());
-  },
-  _zoomHandler: function(e){
-    this.clearLayers();
-    this._renderCells(e.target.getBounds());
-  },
-  _renderCells: function(bounds) {
-    var cells = this._cellsInBounds(bounds);
-    this.fire("newcells", cells);
-    for (var i = cells.length - 1; i >= 0; i--) {
-      var cell = cells[i];
-      if(this._loadedCells.indexOf(cell.id) === -1){
-        (function(cell, i){
-          setTimeout(this.addLayer.bind(this, Leaflet.rectangle(cell.bounds, this.options.style)), this.options.delayFactor*i);
-        }.bind(this))(cell, i);
-        this._loadedCells.push(cell.id);
-      }
-    }
-  },
-  _resizeHandler: function(e) {
-    this._setupSize();
-  },
-  _setupSize: function(){
-    this._rows = Math.ceil(this._map.getSize().x / this._cellSize);
-    this._cols = Math.ceil(this._map.getSize().y / this._cellSize);
-  },
-  _setupGrid: function(bounds){
-    this._origin = this._map.project(bounds.getNorthWest());
-    this._cellSize = this.options.cellSize;
-    this._setupSize();
-    this._loadedCells = [];
-    this.clearLayers();
-    this._renderCells(bounds);
-  },
-  _cellPoint:function(row, col){
-    var x = this._origin.x + (row*this._cellSize);
-    var y = this._origin.y + (col*this._cellSize);
-    return new Leaflet.Point(x, y);
-  },
-  _cellExtent: function(row, col){
-    var swPoint = this._cellPoint(row, col);
-    var nePoint = this._cellPoint(row-1, col-1);
-    var sw = this._map.unproject(swPoint);
-    var ne = this._map.unproject(nePoint);
-    return new Leaflet.LatLngBounds(ne, sw);
-  },
-  _cellsInBounds: function(bounds){
-    var offset = this._map.project(bounds.getNorthWest());
-    var center = bounds.getCenter();
-    var offsetX = this._origin.x - offset.x;
-    var offsetY = this._origin.y - offset.y;
-    var offsetRows = Math.round(offsetX / this._cellSize);
-    var offsetCols = Math.round(offsetY / this._cellSize);
-    var cells = [];
-    for (var i = 0; i <= this._rows; i++) {
-      for (var j = 0; j <= this._cols; j++) {
-        var row = i-offsetRows;
-        var col = j-offsetCols;
-        var cellBounds = this._cellExtent(row, col);
-        var cellId = row+":"+col;
-        cells.push({
-          id: cellId,
-          bounds: cellBounds,
-          distance:cellBounds.getCenter().distanceTo(center)
-        });
-      }
-    }
-    cells.sort(function (a, b) {
-      return a.distance - b.distance;
-    });
-    return cells;
-  }
-});
-
-
 
 class D3MarkerCluster extends MapLayer {
   componentWillMount() {
-    this.leafletElement = new Leaflet.VirtualGrid({});
+
+    this.leafletElement = Leaflet.d3SvgOverlay((svg, projection) => {
+
+      let startingPoint = new Leaflet.LatLng(51.51451110408478, -0.12620388576521444);
+      let unit = 0.0001;
+
+      let data = [];
+      for (let r = 0; r < 20; r++) {
+        for (let c = 0; c < 20; c++) {
+          data.push(new Leaflet.LatLng(startingPoint.lat + (r * unit * 4.5), startingPoint.lng + (c * unit * 7)));
+        }
+      }
+
+      svg
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (i) => {
+          return projection.latLngToLayerPoint(i).x
+        })
+        .attr("y", (i) => {
+          return projection.latLngToLayerPoint(i).y
+        })
+        .attr("height", 0.5 * (1 / projection.scale))
+        .attr("width", 0.5 * (1 / projection.scale))
+        .attr("opacity", 0.2)
+
+    });
+
     this.leafletElement.addTo(this.context.map);
   }
 
@@ -142,7 +51,6 @@ class D3MarkerCluster extends MapLayer {
   }
 }
 
-D3MarkerCluster.defaultProps = {
-}
+D3MarkerCluster.defaultProps = {}
 
 export default D3MarkerCluster;
