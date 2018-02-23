eb576ef0b27a5e0ddc484596d849e8941e2ef4e4
Tom Hewitt <tom.hewitt@o2.com>
2018-02-23 16:02:59 +0000

    Summary card styles

--- src/components/Store/Attraction/SummaryCard/ColorThing.js
+++ src/components/Store/Attraction/SummaryCard/ColorThing.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './ColorThing.less';
 
-const ColorThing = ({children, color='red', text}) => {
+const ColorThing = ({ children, color = 'red', text }) => {
   return (
     <span >
-      <div  style={{'background': color, 'color' : text}} className={styles.arrow_box}>{children}
-      <div  style={{'borderLeftColor': color}} className={styles.arrow_box_arrow}/>
+      <div style={{ background: color, color: text }} className={styles.arrow_box}>{children}
+        <div
+          style={{ borderTopColor: color, borderRightColor: color, borderBottomColor: color }}
+          className={styles.arrow_box_arrow_tail}
+        />
+        <div style={{ borderLeftColor: color }} className={styles.arrow_box_arrow} />
       </div>
-
     </span>
-
   );
 };
 
