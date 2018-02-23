38905ce48d9e538cae767e2725630cba3c5f9e0b
Graham <mogmog@gmail.com>
2018-02-23 10:29:40 +0000

    moved styles into class

--- src/components/Common/SideMenu/index.js
+++ src/components/Common/SideMenu/index.js
@@ -14,25 +14,25 @@ class SideMenu extends PureComponent {
     return (
       <div className={styles.menuContainer}>
       <Menu>
-        <Menu.Item style={{ width: 80, height: 80, lineHeight: 80, padding: 0, margin: 0 }}>
+        <Menu.Item className={styles.menuItem}>
           <Link to="/store/attraction">
             <div className={[styles.icon, styles.icFunnel].join(' ')} />
           </Link>
         </Menu.Item>
 
-        <Menu.Item style={{ width: 80, height: 80, lineHeight: 80, padding: 0, margin: 0 }}>
+        <Menu.Item className={styles.menuItem}>
           <Link to="/store/attraction2">
             <div className={[styles.icon, styles.icFunnel].join(' ')} />
           </Link>
         </Menu.Item>
 
-        <Menu.Item style={{ width: 80, height: 80, lineHeight: 80, padding: 0, margin: 0 }}>
+        <Menu.Item className={styles.menuItem}>
           <Link to="/store/battleground">
             <div className={[styles.icon, styles.icBattleground].join(' ')} />
           </Link>
         </Menu.Item>
 
-        <Menu.Item style={{ width: 80, height: 80, lineHeight: 80, padding: 0, margin: 0 }}>
+        <Menu.Item className={styles.menuItem}>
           <Link to="/store/fullperspective">
             <div className={[styles.icon, styles.icFullPerspective].join(' ')} />
           </Link>
