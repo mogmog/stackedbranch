c38879f7a210250cc85dd5e4758f68f83997cf33
Tom Hewitt <tom.hewitt@o2.com>
2018-02-27 12:09:35 +0000

    Login style fix

--- src/routes/User/Login.js
+++ src/routes/User/Login.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {connect} from 'dva';
-import {routerRedux, Link} from 'dva/router';
-import {Form, Input, Tabs, Button, Icon, Checkbox, Row, Col, Alert} from 'antd';
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { routerRedux, Link } from 'dva/router';
+import { Form, Input, Tabs, Button, Icon, Checkbox, Row, Col, Alert } from 'antd';
 import styles from './Login.less';
-import {Layout} from "antd/lib/index";
+import { Layout } from 'antd/lib/index';
 import LucaButton from './../../components/LucaUI/Button/LucaButton';
 
 const FormItem = Form.Item;
-const {TabPane} = Tabs;
+const { TabPane } = Tabs;
 const { Header, Sider, Content } = Layout;
 
 @connect(state => ({
@@ -35,10 +35,10 @@ export default class Login extends Component {
 
   onGetCaptcha = () => {
     let count = 59;
-    this.setState({count});
+    this.setState({ count });
     this.interval = setInterval(() => {
       count -= 1;
-      this.setState({count});
+      this.setState({ count });
       if (count === 0) {
         clearInterval(this.interval);
       }
@@ -47,10 +47,9 @@ export default class Login extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const {type} = this.state;
-    this.props.form.validateFields({force: true},
+    const { type } = this.state;
+    this.props.form.validateFields({ force: true },
       (err, values) => {
-
         if (this.props.form.getFieldValue('userName') === 'graham.bates@biggroup.co.uk') {
           this.props.dispatch(routerRedux.push('/store/attraction'));
         } else {
@@ -72,7 +71,7 @@ export default class Login extends Component {
   renderMessage = (message) => {
     return (
       <Alert
-        style={{marginBottom: 24}}
+        style={{ marginBottom: 24 }}
         message={message}
         type="error"
         showIcon
@@ -81,39 +80,39 @@ export default class Login extends Component {
   }
 
   render() {
-    const {form, login} = this.props;
-    const {getFieldDecorator} = form;
-    const {count, type} = this.state;
+    const { form, login } = this.props;
+    const { getFieldDecorator } = form;
+    const { count, type } = this.state;
     return (
 
-      <Content style={{position:'absolute', top:0, padding: '10% 24px 0', height: '100%', background: 'url(' + require('../../assets/img/image-bg.png') + ')', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize : 'cover'  }}>
+      <Content style={{ position: 'absolute', top: 0, padding: '150px 0', height: '100%', width: '100%', background: `url(${require('../../assets/img/image-bg.png')})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}>
         <div className="login" >
-          <div className={styles.main}  >
+          <div className={styles.main} >
 
             <div className={styles.borderthing}>
-              <img style={{'width': '100px'}} src={require('../../assets/img/luca.png')}/>
+              <img style={{ width: '100px' }} src={require('../../assets/img/luca.png')} />
             </div>
 
-            <div style={{'textAlign' : 'center', paddingTop : '34px' }}>
-            <h3>Welcome to</h3>
-            <h1 style={{marginTop: '-24px'}}>Smartsteps</h1>
+            <div style={{ textAlign: 'center', paddingTop: '34px' }}>
+              <h3>Welcome to</h3>
+              <h1 style={{ marginTop: '-24px' }}>Smart Steps</h1>
             </div>
-            <Form style={{'padding' : '5%'}} onSubmit={this.handleSubmit} hideRequiredMark>
+            <Form style={{ padding: '5%' }} onSubmit={this.handleSubmit} hideRequiredMark>
 
-              <FormItem label={"Username"}>
+              <FormItem label="Username">
                 {getFieldDecorator('userName', {
                   rules: [{
-                    required: type === 'account', message: 'You must enter a user name'
+                    required: type === 'account', message: 'You must enter a user name',
                   }],
                 })(
                   <Input
                     size="large"
-                    prefix={<Icon type="user" className={styles.prefixIcon}/>}
+                    prefix={<Icon type="user" className={styles.prefixIcon} />}
                     placeholder="user name is an email"
                   />
                 )}
               </FormItem>
-              <FormItem label={'Password'} >
+              <FormItem label="Password" >
                 {getFieldDecorator('password', {
                   rules: [{
                     required: type === 'account', message: 'Enter a password',
@@ -121,18 +120,18 @@ export default class Login extends Component {
                 })(
                   <Input
                     size="large"
-                    prefix={<Icon type="lock" className={styles.prefixIcon}/>}
+                    prefix={<Icon type="lock" className={styles.prefixIcon} />}
                     type="password"
                     placeholder="password here"
                   />
                 )}
               </FormItem>
 
-              <FormItem style={{'textAlign' : 'center'}} className={styles.additional}>
+              <FormItem style={{ textAlign: 'center' }} className={styles.additional}>
                 <LucaButton size="large" type="primary" htmlType="submit">Sign in</LucaButton>
               </FormItem>
 
-              <FormItem style={{'textAlign' : 'center'}}>
+              <FormItem style={{ textAlign: 'center' }}>
                 <a className="login-form-forgot" href="">Forgot password</a>
               </FormItem>
             </Form>
