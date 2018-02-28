5e46544a4fefc3aa8652b89cf708023fc7ffefc0
Graham <mogmog@gmail.com>
2018-02-28 14:51:28 +0000

    redirect to login

--- src/router.js
+++ src/router.js
@@ -82,7 +82,7 @@ function RouterConfig({ history, app }) {
           <Router history={history}>
             <Switch>
 
-              <Redirect exact from="/" to="/store/attraction" />
+              <Redirect exact from="/" to="/login" />
               <Route path="/login" render={props => <Login submitting={false} />} />
               <Route path="/store/" render={props => <StoreLayout {...props} {...storepassProps} />} />
               <Route path="/travel/" render={props => <TravelLayout {...props} {...travelpassProps} />} />
